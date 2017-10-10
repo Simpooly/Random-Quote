98fc9f8003f7bebad2b3d167dc11bf9d4cab3aad
Krista <krista.t.jekel@gmail.com>
2017-10-10 14:11:01 -0600

    print data to console

--- randomQuote.js
+++ randomQuote.js
@@ -1,6 +1,19 @@
 
   $(document).ready(function() {
+    
+ function getQuote(){
+    $.ajax({
+        dataType: 'json',
+        url: 'https://talaikis.com/api/quotes/random/',
+        success: function(data){
+         // $('.quote').html("data.quote");
+         //$('.author').html("data.author");
+          console.log(data);
+        }
+    });
+ }
+    
     $("#getmessage").on("click",function(){
-      $(".quote").html("You got this!");
+      getQuote();
     });
   });
